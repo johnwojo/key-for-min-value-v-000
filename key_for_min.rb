0e59e4af3b7e5b86dc609f44ebb3ae72cd566306
John Wojciechowski <jwojo13@gmail.com>
2017-11-17 20:44:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,10 @@
 
 def key_for_min_value(name_hash)
   collection = []
-  name_hash.each do |key,value| collection << value
-    collection.collect do |a,b,c|
-      a <=> b <=> c
+  name_hash.capture do |key,value|
+
   end
-  end
+  return value
 end
+
+
