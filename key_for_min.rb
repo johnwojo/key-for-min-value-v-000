1c658fb5e5d0795dbcaa029298127645306c7188
John Wojciechowski <jwojo13@gmail.com>
2017-11-17 20:44:55 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,12 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  collection = []
+def key_for_min_value(name_hash)s
   name_hash.capture do |key,value|
 
   end
   return value
 end
-
-
