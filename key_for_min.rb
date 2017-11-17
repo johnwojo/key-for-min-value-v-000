cb4f0375d6dc28f60ee193187165b71bc3d79b51
John Wojciechowski <jwojo13@gmail.com>
2017-11-17 21:00:54 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@
 
 
 def key_for_min_value(name_hash)
-min_key = nil
-min_value = nil
+min_key = 0
+min_value = 0
   name_hash.each do |key,value|
-    if min_value == nil || value < min_value
+    if min_value == 0 || value < min_value
       min_key = key
       min_value = value
     end
